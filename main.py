215f402b562802f52a1cd3f208ff51b37c8991b0
Python <arina.s.anikeeva@mail.ru>
2024-11-28 20:02:28 +0300

    Circle

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
-print("My first git-repo")
+import sys
+import random
+from PyQt6 import uic  # Импортируем uic
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False # Загружаем дизайн
+        self.pushButton.clicked.connect(self.paint)
+        # Обратите внимание: имя элемента такое же как в QTDesigner
+
+    def eventrun(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_circle(self, qp):
+        r = random.randint(20, 80)
+        x = random.randint(100, 200)
+        y = random.randint(20, 100)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(x, y, r, r)
+
+        # Имя элемента совпадает с objectName в QTDesigner
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec())
